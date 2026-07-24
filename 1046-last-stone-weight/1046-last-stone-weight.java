b1840b0542d6f6c4a08a31794f277dbda1813fee
Mangesh shinde <149881322+Mangesh5677@users.noreply.github.com>
2026-07-24 18:49:04 +0530

    Time: 2 ms (34.98%), Space: 43.2 MB (15%) - LeetHub

---
+++ 1046-last-stone-weight/1046-last-stone-weight.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int lastStoneWeight(int[] stones) {
+        int n = stones.length;
+
+        if (n == 1) {
+            return stones[0];
+        }
+        
+        while (true) {
+            Arrays.sort(stones);
+            if (stones[n - 2] == 0) {
+                return stones[n - 1];
+            }
+            stones[n - 2] = stones[n - 1] - stones[n - 2];
+            stones[n - 1] = 0;
+        }
+    }
+}
